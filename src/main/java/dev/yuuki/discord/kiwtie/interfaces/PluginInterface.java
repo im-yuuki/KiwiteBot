6dbfeaec7e557a0263d092a6978a0d83f4f689dd
Yuuki <yuukii.dn@gmail.com>
2024-06-11 22:36:45 +0700

    seg

--- src/main/java/dev/yuuki/discord/kiwtie/interfaces/PluginInterface.java
+++ src/main/java/dev/yuuki/discord/kiwtie/interfaces/PluginInterface.java
@@ -1,5 +1,7 @@
 package dev.yuuki.discord.kiwtie.interfaces;
 
+import dev.yuuki.discord.bot.interfaces.InstanceManagerInterface;
+
 public interface PluginInterface {
     String name = null;
     String version = null;
